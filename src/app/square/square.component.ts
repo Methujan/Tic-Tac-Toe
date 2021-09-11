d08cf71ac538b676797a3c177a139db082ef4a76
Methujan <methujan_02@hotmail.com>
2021-09-10 22:44:07 -0400

    displays X and O when grid squares are clicked

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,7 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
+  template: `  <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>`,
   styleUrls: ['./square.component.css']
 })
 export class SquareComponent {
