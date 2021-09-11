333dc87e78aa241741b841a33e7fdeb620a1c92b
Methujan <methujan_02@hotmail.com>
2021-09-10 22:17:05 -0400

    generate board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BoardComponent } from './board/board.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
