128169e30f81d00c0f74e098d74b06a46dc2a382
Methujan <methujan_02@hotmail.com>
2021-09-10 23:03:18 -0400

    removed styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { SquareComponent } from './square/square.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BoardComponent } from './board/board.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,13 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
